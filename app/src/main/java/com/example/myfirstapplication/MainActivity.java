65b351158f5a352a1e636eba7985eb1c4081bf99
joting <esjoting@gmail.com>
2023-03-21 11:42:28 +0800

    V1

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String heroName;
+        int number1 = 10;
+        double number2 = 10.1;
+    }
+    public int hp(){
+        return 0;
+    }
+}
